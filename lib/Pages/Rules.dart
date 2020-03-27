11c97d7613d465d7c14d9a7f688b8fcc1b5a1323
mennaabdelmoneim <45489009+mennaabdelmoneim@users.noreply.github.com>
2020-03-27 04:37:49 +0200

    Rule_UI
    this's Rule_UI

---
+++ lib/Pages/Rules.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'AddRoles.dart';
+import 'AddRules.dart';
+
+class Rules extends StatefulWidget {
+  @override
+ State<StatefulWidget> createState() {
+    return new _Rules();
+  }
+}
+
+class _Rules extends State<Rules> {
+  @override
+  Widget build(BuildContext context) {
+    
+    return new Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent, 
+        onPressed: (){
+            showDialog(
+              barrierDismissible: false,
+              context: context,
+              builder: (BuildContext context) => AddRoles(),);
+        },
+        child: Icon(Icons.add ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      appBar: new AppBar(
+        backgroundColor: Colors.grey[700],
+        title: new Text("الأفوكاتو", style: new TextStyle(fontSize: 21,fontWeight: FontWeight.bold),),
+      ),);
+  }
+  
+  
+  }
+
